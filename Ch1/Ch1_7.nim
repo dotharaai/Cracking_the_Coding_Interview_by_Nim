ff2625cc7836a47246a26a165ad1dae95932236e
kshimada <dot.haraai@gmail.com>
2018-04-04 17:30:34 +0900

    in-placeに変更

--- Ch1/Ch1_7.nim
+++ Ch1/Ch1_7.nim
@@ -8,19 +8,20 @@ type
   Image[N:static[int]] = array[1..N,array[1..N,int]]
 
 
-proc Ch1_7(i:Image):Image=
+proc Ch1_7(i:var Image)=
   for x in 1..len(i):
-    for y in 1..len(i):
-      result[y][x] = i[x][y]
+    for y in 1..<x:
+      swap(i[x][y],i[y][x])
 
 
 import unittest
 suite "description for this stuff":
 
   test "rotate":
-    let image:Image[3] = [[1,2,3],[0,0,0],[0,0,0]]
+    var image:Image[3] = [[1,2,3],[0,0,0],[0,0,0]]
+    Ch1_7(image)
     # give up and stop if this fails
     check:
-      Ch1_7(image) == [[1,0,0],[2,0,0],[3,0,0]]
+      image == [[1,0,0],[2,0,0],[3,0,0]]
 
   echo "suite teardown: run once after the tests"
